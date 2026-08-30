ef663edd1b773427638f9003650f0021761104ac
shaman2016scratch <g.ilkh@yandex.ru>
2026-08-30 20:37:52 +0300

    fix

--- src/lib/translations/index.js
+++ src/lib/translations/index.js
@@ -2,7 +2,7 @@ const translations = {
     en: {
         settings: "settings",
         status: "status",
-        "main.label": "{name}} - Messenger of new gen!",
+        "main.label": "{name} - Messenger of new gen!",
         "main.webclient.link": "Use web version",
         "footer.platform": "Official, {name} v{version}",
         "login.title": "Login",
@@ -13,7 +13,7 @@ const translations = {
     ru: {
         settings: "настройки",
         status: "статус",
-        "main.label": "[name] - Мессенджер нового поколения!",
+        "main.label": "{name} - Мессенджер нового поколения!",
         "main.webclient.link": "Использовать веб-версию",
         "footer.platform": "Официально, {name} v{version}",
         "login.title": "Вход",
